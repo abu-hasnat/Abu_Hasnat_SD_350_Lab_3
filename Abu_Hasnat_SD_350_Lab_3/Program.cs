1271feea054b386d7813875aeb5b6d174a556c9c
Abu Hasnat <hasnat.tushar@gmail.com>
2023-10-25 21:32:55 -0500

    Add project files.

---
+++ Abu_Hasnat_SD_350_Lab_3/Program.cs
@@ -0,0 +1,61 @@
+﻿public abstract class User
+{
+    public string Password { get; set; }
+    public abstract string PasswordHash();
+}
+
+public class AuthorizeUser : User
+{
+    public override string PasswordHash()
+    {
+        return Password + "Authorized";
+    }
+}
+
+public class Administrator : User
+{
+    public override string PasswordHash()
+    {
+        return Password + "Administrator";
+    }
+}
+
+public abstract class System
+{
+    protected abstract User CreateUser(string JSON);
+}
+
+public class TwoFactorRequired: System
+{
+    protected override User CreateUser(string JSON)
+    {
+        if(JSON == "TwoFactorAuthentication: true")
+        {
+            return new AuthorizeUser();
+        }
+        else if(JSON == "IsAdmin: true")
+        {
+            return new Administrator();
+        }
+       // throw new Exception("Invalid");
+
+
+    }
+}
+
+public class TwoFactorNotRequired : System
+{
+    protected override User CreateUser(string JSON)
+    {
+        if(JSON == "TwoFactorAuthenticatio: true")
+        {
+            return new AuthorizeUser();
+        }
+        else if(JSON == "IsAdmin: true")
+        {
+            return new Administrator();
+        }
+
+        return new AuthorizeUser();
+    }
+}
